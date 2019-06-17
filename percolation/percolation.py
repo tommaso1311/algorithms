04006992192bd9628330c5eb814c52baa02dd0b2
tommaso1311 <tommaso131195@gmail.com>
2019-06-17 16:47:07 +0200

    Created percolation template
    Added the suggested methods to the percolation class

---
+++ percolation/percolation.py
@@ -0,0 +1,26 @@
+class percolation:
+
+
+	def __init__(self, N):
+
+		pass
+
+
+	def open(self, row, column):
+
+		pass
+
+
+	def is_open(self, row, column):
+
+		pass
+
+
+	def is_full(self, row, column):
+
+		pass
+
+
+	def percolates(self):
+
+		pass
