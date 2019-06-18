ffd186a3c543e2982817cf86031a8a68ab5575fc
tommaso1311 <tommaso131195@gmail.com>
2019-06-18 23:15:26 +0200

    Added weights to union

--- percolation/percolation.py
+++ percolation/percolation.py
@@ -6,6 +6,7 @@ class percolation:
 	def __init__(self, N):
 
 		self.sites = np.array([i for i in range(N+2)])
+		self.sizes = np.ones(N+2, int)
 
 
 	def open(self, row, column):
@@ -43,19 +44,28 @@ class percolation:
 		i = self.root(p)
 		j = self.root(q)
 
-		self.sites[i] = j
+		if i == j:
+			pass
 
+		if self.sizes[i] < self.sizes[j]:
+			self.sites[i] = j
+			self.sizes[j] += self.sizes[i]
+		else:
+			self.sites[j] = i
+			self.sizes[i] += self.sizes[j]
 
 
 def main():
 
 	p = percolation(10)
 	print(p.sites)
+	print(p.sizes)
 
-	p.union(3, 8)
+	p.union(8, 3)
 	p.union(8, 5)
 
 	print(p.sites)
+	print(p.sizes)
 
 if __name__ == "__main__":
 	main()
