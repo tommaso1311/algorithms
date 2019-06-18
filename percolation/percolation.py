0548e9fe0f1e6beb898d4f26e721f352f0ba3f71
tommaso1311 <tommaso131195@gmail.com>
2019-06-18 23:33:22 +0200

    Corrected initializer

--- percolation/percolation.py
+++ percolation/percolation.py
@@ -5,8 +5,8 @@ class percolation:
 
 	def __init__(self, N):
 
-		self.sites = np.array([i for i in range(N+2)])
-		self.sizes = np.ones(N+2, int)
+		self.sites = np.array([i for i in range(N*N+2)])
+		self.sizes = np.ones(N*N+2)
 
 
 	def open(self, row, column):
@@ -28,6 +28,7 @@ class percolation:
 
 		pass
 
+
 	def root(self, i):
 
 		while i != self.sites[i]:
@@ -36,9 +37,11 @@ class percolation:
 
 		return i
 
+
 	def connected(self, p, q):
 
 		return self.root(p) == self.root(q)
+
 
 	def union(self, p, q):
 
@@ -58,16 +61,10 @@ class percolation:
 
 def main():
 
-	p = percolation(10)
+	p = percolation(4)
 	print(p.sites)
 	print(p.sizes)
 
-	p.union(8, 3)
-	p.union(8, 5)
-	p.union(7, 5)
-
-	print(p.sites)
-	print(p.sizes)
 
 if __name__ == "__main__":
 	main()
